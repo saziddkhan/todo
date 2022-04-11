1e04642cbbe9378c2c55d5b4b7a6d612cac5ee98
sazid <96243953+saziddkhan@users.noreply.github.com>
2022-04-11 22:39:14 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <todo-list class="name" list-name="TODO"  />
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoList,
+  },
+};
+</script>
---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,30 @@
+<template>
+  <form class="form" @submit.prevent="addTodo()">
+    <input
+      v-model="newTodo"
+      type="text"
+      class="input"
+      placeholder="to-do..."
+    />
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.length > 0) {
+        this.$emit("on-new-todo", this.newTodo);
+      }
+      this.newTodo = "";
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
